4877f0391d7a5f2e8db02d6417d257a25f9a4683
theAmazingByte <ksamvid@gmail.com>
2020-11-21 15:24:53 -0500

    Made Database

--- database/database.py
+++ database/database.py
@@ -1,3 +1,22 @@
 import sqlite3 as sql
 
 conn = sql.connect('database/myData.db')
+
+# conn.execute('''CREATE TABLE ITINERARY
+#          (id INT PRIMARY KEY     NOT NULL,
+#          name           TEXT    NOT NULL,
+#          days STRING  NOT NULL);''')
+
+# conn.execute('''CREATE TABLE DAY
+#          (dayID INT PRIMARY KEY     NOT NULL,
+#          name           TEXT    NOT NULL,
+#          events  STRING NOT NULL);''')
+
+# conn.execute('''CREATE TABLE EVENT
+#          (eventID INT PRIMARY KEY     NOT NULL,
+#          type             TEXT      NOT NULL,
+#          timestarted      TEXT    NOT NULL,
+#          timeended        TEXT    NOT NULL);''')
+
+
+conn.close()
