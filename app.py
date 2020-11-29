a15d50e6b12c8d2986f56f4be55cc23db7eafb00
theAmazingByte <ksamvid@gmail.com>
2020-11-29 12:26:10 -0500

    Started Creating Itinerary.html

--- app.py
+++ app.py
@@ -1,53 +1,62 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import sqlite3
 
 app = Flask('__name__')
+app.secret_key = '(svfq%$&$*&%^&'
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-itineraryInUse = (0)
-
 # * Routes
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/itinerary')
 def itinerary():
-    global itineraryInUse
-    return render_template('itinerary.html', itineraryInUse = itineraryInUse)
+    itineraryInUseID = session['itineraryInUse']
+    print(itineraryInUseID)
+    conn = sqlite3.connect('database/myData.db')
+    itineraryInUseForHtml = conn.execute(
+        "SELECT id, name from ITINERARY")
+    for row in itineraryInUseForHtml:
+        if row[0] == int(itineraryInUseID):
+            myName = row[1]
+    return render_template('itinerary.html', itineraryInUseID=itineraryInUseID, myName = myName)
+
 
 @app.route('/getuniqueid')
 def getuniqueid(methods=['GET', 'POST']):
     givemethenextid = request.args.get('givemethenextid')
     print('givemethenextid from request:  '+givemethenextid)
-    if givemethenextid == 'true' :
+    if givemethenextid == 'true':
         conn = sqlite3.connect('database/myData.db')
         cursor = conn.execute("SELECT currentID from CURRENT")
         for row in cursor:
-            print ("CurrentID is ", row[0])
+            print("CurrentID is ", row[0])
             currentId = str(row[0])
 
     return currentId
 
+
 @app.route('/finaladdbutton')
 def finaladdbutton(methods=['GET', 'POST']):
-    global itineraryInUse
     name = request.args.get('name')
     print('name from request:  '+name)
     conn = sqlite3.connect('database/myData.db')
 
     cursorcurrentid = conn.execute("SELECT currentID from CURRENT")
     for row in cursorcurrentid:
-        print ("CurrentID is ", row[0])
+        print("CurrentID is ", row[0])
         currentId = str(row[0])
     cursordayid = conn.execute("SELECT currentdayID from CURRENTDAY")
     for row in cursordayid:
-        print ("CurrentdayID is ", row[0])
+        print("CurrentdayID is ", row[0])
         currentdayid = str(row[0])
     cursoreventid = conn.execute("SELECT currenteventID from CURRENTEVENT")
     for row in cursoreventid:
-        print ("CurrenteventID is ", row[0])
+        print("CurrenteventID is ", row[0])
         currenteventid = str(row[0])
 
     conn.execute("INSERT INTO ITINERARY (id, name) \
@@ -61,17 +70,23 @@ def finaladdbutton(methods=['GET', 'POST']):
     newcurrentdayid = str(int(currentdayid) + 1)
     newcurrenteventid = str(int(currenteventid) + 1)
 
-    conn.execute("UPDATE CURRENT set currentID = ? where currentID = ?", (newcurrentid, currentId))
-    conn.execute("UPDATE CURRENTDAY set currentdayID = ? where currentdayID = ?", (newcurrentdayid, currentdayid))
-    conn.execute("UPDATE CURRENTEVENT set currenteventID = ? where currenteventID = ?", (newcurrenteventid, currenteventid))
+    conn.execute("UPDATE CURRENT set currentID = ? where currentID = ?",
+                 (newcurrentid, currentId))
+    conn.execute("UPDATE CURRENTDAY set currentdayID = ? where currentdayID = ?",
+                 (newcurrentdayid, currentdayid))
+    conn.execute("UPDATE CURRENTEVENT set currenteventID = ? where currenteventID = ?",
+                 (newcurrenteventid, currenteventid))
 
     conn.commit()
-    itineraryInUse = (currentId)
+
+    session['itineraryInUse'] = currentId
 
     return "thisreturnstatementdoesnotmatter"
 
+
 if __name__ == '__main__':
-    app.run(debug = True, host = '0.0.0.0') # ! Remove debug = True after development
+    # ! Remove debug = True after development
+    app.run(debug=True, host='0.0.0.0')
 
 # * Ajax Template
 # @app.route('/ajaxroute') #! Add Route
--- database/database.py
+++ database/database.py
@@ -2,40 +2,40 @@ import sqlite3 as sql
 
 conn = sql.connect('database/myData.db')
 
-conn.execute('''CREATE TABLE ITINERARY
-          (id INT PRIMARY KEY     NOT NULL,
-          name           TEXT    NOT NULL);''')
+# conn.execute('''CREATE TABLE ITINERARY
+#           (id INT PRIMARY KEY     NOT NULL,
+#           name           TEXT    NOT NULL);''')
 
-conn.execute('''CREATE TABLE DAY
-        (dayid INT PRIMARY KEY  NOT NULL,
-        itineraryid INT NOT NULL,  
-        name           TEXT    NOT NULL);''')
+# conn.execute('''CREATE TABLE DAY
+#         (dayid INT PRIMARY KEY  NOT NULL,
+#         itineraryid INT NOT NULL,  
+#         name           TEXT    NOT NULL);''')
 
-conn.execute('''CREATE TABLE EVENT
-          (dayID         INT     NOT NULL,
-          type             TEXT      NOT NULL,
-          timestarted      TEXT    NOT NULL,
-          timeended        TEXT    NOT NULL,
-          eventid         INT  PRIMARY KEY    NOT NULL);''')
+# conn.execute('''CREATE TABLE EVENT
+#           (dayID         INT     NOT NULL,
+#           type             TEXT      NOT NULL,
+#           timestarted      TEXT    NOT NULL,
+#           timeended        TEXT    NOT NULL,
+#           eventid         INT  PRIMARY KEY    NOT NULL);''')
 
-conn.execute('''CREATE TABLE CURRENT
-          (currentID INT  NOT NULL);''')
+# conn.execute('''CREATE TABLE CURRENT
+#           (currentID INT  NOT NULL);''')
 
-conn.execute('''CREATE TABLE CURRENTDAY
-          (currentdayID INT  NOT NULL);''')
+# conn.execute('''CREATE TABLE CURRENTDAY
+#           (currentdayID INT  NOT NULL);''')
 
-conn.execute('''CREATE TABLE CURRENTEVENT
-          (currenteventID INT  NOT NULL);''')
+# conn.execute('''CREATE TABLE CURRENTEVENT
+#           (currenteventID INT  NOT NULL);''')
 
-conn.execute("INSERT INTO CURRENT (currentID) \
-      VALUES (1)")
+# conn.execute("INSERT INTO CURRENT (currentID) \
+#       VALUES (1)")
 
-conn.execute("INSERT INTO CURRENTDAY (currentdayID) \
-      VALUES (1)")
+# conn.execute("INSERT INTO CURRENTDAY (currentdayID) \
+#       VALUES (1)")
 
-conn.execute("INSERT INTO CURRENTEVENT (currenteventID) \
-      VALUES (1)")
+# conn.execute("INSERT INTO CURRENTEVENT (currenteventID) \
+#       VALUES (1)")
 
-conn.commit()
+# conn.commit()
 
 conn.close()
